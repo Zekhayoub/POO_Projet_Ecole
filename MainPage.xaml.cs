26e8c696377067a2f1e3cfb0aa0d03e027e78f1c
guilluy <baptisteguilluy0728@gmail.com>
2023-12-13 15:01:04 +0100

    premier test

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -11,15 +11,14 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-		if (File.Exists(_fileName))
-            TextEditor.Text = File.ReadAllText(_fileName);
+		
 	}
 
 	private void OnAddStudentClicked(object sender, EventArgs e)
 	{	
 		var Pagestudent = new Studentnew();
 		Navigation.PushAsync(Pagestudent);
-		File.WriteAllText(_fileName, TextEditor.Text);
+		
     } 
 	// }
 	// private void OnAddActivityClicked(object sender, EventArgs e)
--- Studentnew.xaml.cs
+++ Studentnew.xaml.cs
@@ -5,5 +5,6 @@ public partial class Studentnew : ContentPage
 	public Studentnew()
 	{
 		InitializeComponent();
+		
 	}
 }
