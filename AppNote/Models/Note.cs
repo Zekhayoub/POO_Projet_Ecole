9c6b60669e7ea3d26bb7e6f9c98e695fb37e8a41
zekhayoub <22371@ecam.be>
2023-12-24 15:00:56 +0100

    Rajout d'une fonctionnalité de note et d'activité

--- AppNote/Models/Note.cs
+++ AppNote/Models/Note.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AppNote.Models
+﻿namespace AppNote.Models
 {
     public class Note
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string Activity { get; set; }
+        public string NoteCote { get; set; } // Ajout de la propriété NoteCote
     }
 }
